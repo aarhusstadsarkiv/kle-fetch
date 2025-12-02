57f153b54d1faa81e1db3633ec6d8da00095f459
Kristoffer <prik@aarhus.dk>
2025-12-02 08:41:36 +0100

    split parent class into parts

--- src/kle_fetch/main.py
+++ src/kle_fetch/main.py
@@ -1,72 +1,10 @@
-from functools import lru_cache
-from pydantic import BaseModel
-
-from kle_fetch.fetch import KLEHandler
-
-
-kle_handler = KLEHandler(cache=True)
-
-
-class Emne(BaseModel):
-    JournalNr: str
-    HovedGruppeNr: str
-    HovedGruppeTxt: str
-    GruppeNr: str
-    GruppeTxt: str
-    EmneNr: str
-    EmneTxt: str
-
-    def contains(self, txt: str) -> bool:
-        txt = txt.lower()
-        return (
-            txt in self.HovedGruppeTxt.lower()
-            or txt in self.GruppeTxt.lower()
-            or txt in self.EmneTxt.lower()
-        )
-
-
-@lru_cache
-def get_emner() -> list[Emne]:
-    fetched_emner = kle_handler.get_emner().content
-    # grupper = [
-    #     (f"{g['GruppeNr']} {g['GruppeTitel']}", list(map(int, g["GruppeNr"].split("."))))
-    #     for hg in all_emner["KLE-Emneplan"]["Hovedgruppe"]
-    #     for g in (hg["Gruppe"] if isinstance(hg["Gruppe"], list) else [hg["Gruppe"]])
-    # ]
-
-    emner: list[Emne] = []
-
-    for hg in fetched_emner["KLE-Emneplan"]["Hovedgruppe"]:
-        for g in hg["Gruppe"] if isinstance(hg["Gruppe"], list) else [hg["Gruppe"]]:
-            for e in g["Emne"] if isinstance(g["Emne"], list) else [g["Emne"]]:
-                kle_nums = e["EmneNr"].split(".")
-                emner.append(
-                    Emne(
-                        JournalNr=e["EmneNr"],
-                        HovedGruppeNr=kle_nums[0],
-                        HovedGruppeTxt=hg["HovedgruppeTitel"],
-                        GruppeNr=kle_nums[1],
-                        GruppeTxt=g["GruppeTitel"],
-                        EmneNr=kle_nums[2],
-                        EmneTxt=e["EmneTitel"],
-                    )
-                )
-
-    return sorted(emner, key=lambda e: e.JournalNr)
-
-
-@lru_cache
-def get_facetter() -> list[tuple[str, str]]:
-    all_facetter = kle_handler.get_facetter().content
-    facetter = [
-        (f"{f['HandlingsfacetNr']} {f['HandlingsfacetTitel']}", f["HandlingsfacetNr"])
-        for fk in all_facetter["KLE-Handlingsfacetter"]["HandlingsfacetKategori"]
-        for f in fk["Handlingsfacet"]
-    ]
-    return sorted(facetter, key=lambda e: e[0])
+from kle_fetch.structure import get_emner
 
 
 if __name__ == "__main__":
     emner = get_emner()
-    filtered = list(filter(lambda x: x.contains("virksomhed"), emner))
-    print(filtered[10])
+
+    for emne in emner:
+        if emne.emne.udgaaetdato:
+            print(emne)
+            input()
---
+++ src/kle_fetch/structure.py
@@ -0,0 +1,77 @@
+import typing as t
+
+from functools import lru_cache
+from pydantic import BaseModel
+
+from kle_fetch.fetch import KLEHandler
+
+
+kle_handler = KLEHandler(cache=True)
+
+
+class KLEPart(BaseModel):
+    nr: str
+    txt: str
+    oprettetdato: str
+    udgaaetdato: t.Optional[str] = None
+
+    @classmethod
+    def build(cls, part_dict: dict, part_name: str, kle_num: str) -> KLEPart:
+        return cls(
+            nr=kle_num,
+            txt=part_dict[part_name + "Titel"],
+            oprettetdato=part_dict[part_name + "AdministrativInfo"]["OprettetDato"],
+            udgaaetdato=part_dict[part_name + "AdministrativInfo"]
+            .get("Historisk", {})
+            .get("UdgaaetDato", None),
+        )
+
+
+class KLE(BaseModel):
+    journalnr: str
+
+    hovedgruppe: KLEPart
+    gruppe: KLEPart
+    emne: KLEPart
+
+    @classmethod
+    def build(cls, hg: dict, g: dict, e: dict, kle_nums: list[str]) -> KLE:
+        return cls(
+            journalnr=e["EmneNr"],
+            hovedgruppe=KLEPart.build(hg, "Hovedgruppe", kle_nums[0]),
+            gruppe=KLEPart.build(g, "Gruppe", kle_nums[1]),
+            emne=KLEPart.build(e, "Emne", kle_nums[2]),
+        )
+
+    def contains(self, txt: str) -> bool:
+        txt = txt.lower()
+        return (
+            txt in self.hovedgruppe.txt.lower()
+            or txt in self.gruppe.txt.lower()
+            or txt in self.emne.txt.lower()
+        )
+
+
+@lru_cache
+def get_emner() -> list[KLE]:
+    fetched_emner = kle_handler.get_emner().content
+    emner: list[KLE] = []
+
+    for hg in fetched_emner["KLE-Emneplan"]["Hovedgruppe"]:
+        for g in hg["Gruppe"] if isinstance(hg["Gruppe"], list) else [hg["Gruppe"]]:
+            for e in g["Emne"] if isinstance(g["Emne"], list) else [g["Emne"]]:
+                kle_nums = e["EmneNr"].split(".")
+                emner.append(KLE.build(hg, g, e, kle_nums))
+
+    return sorted(emner, key=lambda e: e.journalnr)
+
+
+@lru_cache
+def get_facetter() -> list[tuple[str, str]]:
+    all_facetter = kle_handler.get_facetter().content
+    facetter = [
+        (f"{f['HandlingsfacetNr']} {f['HandlingsfacetTitel']}", f["HandlingsfacetNr"])
+        for fk in all_facetter["KLE-Handlingsfacetter"]["HandlingsfacetKategori"]
+        for f in fk["Handlingsfacet"]
+    ]
+    return sorted(facetter, key=lambda e: e[0])
